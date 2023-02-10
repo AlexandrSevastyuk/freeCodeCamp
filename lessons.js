0324127908f6d4bfd1405090eb88d9a4fdbd64f7
AlexandrSevastyuk <104134957+AlexandrSevastyuk@users.noreply.github.com>
2023-02-10 19:44:58 +0300

    Update lessons.js

--- lessons.js
+++ lessons.js
@@ -7,3 +7,12 @@ var titleCaseOver;
 studlyCapVar = 10;
 properCamelCase = "A String";
 titleCaseOver = 9000;
+// конкатенация
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks =myAdjective+" "+myNoun+" "+myVerb+" "+myAdverb; // Change this line
+// Only change code above this line
